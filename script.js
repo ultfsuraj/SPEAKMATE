90e4adf103edd6461d6d9d4f8945d30c8eb06575
R-B-Suraj <rb.suraj1kanha@gmail.com>
2024-06-09 19:01:19 +0530

    feat: things working fine in mobile

--- script.js
+++ script.js
@@ -13,17 +13,6 @@ let pitch = 0.95
 let rate = 1
 let voices = window.speechSynthesis.getVoices() || []
 
-const tellMe = (text, voiceIndex, pitch, rate) => {
-    let utterThis = new SpeechSynthesisUtterance(text);
-    utterThis.voice = voices[voiceIndex];
-    utterThis.pitch = pitch;
-    utterThis.rate = rate;
-    window.speechSynthesis?.speak(utterThis);
-    console.log(text)
-}
-
-
-
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 if (!SpeechRecognition) {
@@ -37,17 +26,35 @@ const startBtn = document.querySelector('.startBtn')
 const stopBtn = document.querySelector('.stopBtn')
 const speechTxt = document.querySelector('.speechTxt')
 const interimTxt = document.querySelector('.interim')
-
+const cancelBtn = document.querySelector('.cancelBtn')
 
 const BUFFER = {
     length: 0,
     text: ''
 }
 
+
+const tellMe = (text, voiceIndex, pitch, rate) => {
+    let utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.voice = voices[voiceIndex];
+    utterThis.pitch = pitch;
+    utterThis.rate = rate;
+    utterThis.onstart = () => {
+        recognition.stop()
+    }
+    utterThis.onend = () => {
+        recognition.start()
+    }
+
+    window.speechSynthesis?.speak(utterThis);
+}
+
+
 recognition.onstart = () => {
     startBtn.disabled = true
     stopBtn.disabled = false
 };
+
 
 // here we want to add to textarea, only if event.results[i][0].isFinal
 recognition.onresult = (event) => {
@@ -61,21 +68,31 @@ recognition.onresult = (event) => {
 recognition.onerror = (event) => {
     console.log('Speech recognition error. ', event.error);
     tellMe("speech recognition error. check your internet.", 1, 0.95, 1)
+    recognition.start()
 };
 
 recognition.onend = () => {
-    console.log(BUFFER.text)
-    speechTxt.value += BUFFER.text + ". "
-    setTimeout(()=>interimTxt.textContent = "", 700)
+    BUFFER.text.trim()
+    let index = BUFFER.text.toLocaleLowerCase().indexOf("stop recognition")
+    if (index >= 0) {
+        speechTxt.value += BUFFER.text.substring(0, index)
+    } else {
+        speechTxt.value += BUFFER.text + ". "
+    }
+    setTimeout(() => interimTxt.textContent = "", 700)
     startBtn.disabled = false;
     stopBtn.disabled = true;
-    if(BUFFER.text.toLowerCase().includes("stop recognition")){
+    if (BUFFER.text.toLowerCase().includes("stop recognition")) {
         recognition.stop();
-    }
-    else{
-        recognition.start();
+        if (speechTxt.value.length > 0) {
+            sendMessage(speechTxt.value)
+            BUFFER.text = ''
+        }
+        return
     }
     
+    BUFFER.text = ''
+    recognition.start();
 };
 
 startBtn.addEventListener('click', () => {
@@ -87,21 +104,55 @@ stopBtn.addEventListener('click', () => {
 });
 
 
+cancelBtn.addEventListener('click', () => {
+    window.speechSynthesis.cancel()
+    recognition.start()
+})
 
 speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
-    tellMe(inputTxt,4,0.95,1)
+    tellMe(inputTxt, 4, 0.95, 1)
 }
 
 
 const speakBtn = document.querySelector(".speakBtn")
 
-speakBtn.addEventListener("click",()=>{
+speakBtn.addEventListener("click", () => {
     tellMe(speechTxt.value, 7, 0.95, 1)
 })
 
 const clearBtn = document.querySelector(".clearBtn")
-clearBtn.addEventListener("click",()=>{
+clearBtn.addEventListener("click", () => {
     speechTxt.value = ""
 })
+
+
+
+const sendMessage = async (prompt) => {
+    let payload = {
+        prompt: prompt + " correct above question & Answer in few sentences."
+    }
+    try {
+        const response = await fetch('https://withered-frog-d5b7.purkufirte.workers.dev/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+
+        const data = await response.json()
+        speechTxt.value = ""
+        tellMe(data?.result?.response, 7, 0.95, 1)
+
+    } catch (error) {
+        console.error('Error:', error)
+    }
+
+}
+
 
